d4c3f176295f72a3f5a06ed210f2c58e73f1d753
Tiago Ribeiro <tiagofr98@gmail.com>
2020-12-28 15:57:50 -0300

    Directives
    Dropdown funcionando

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,31 @@
+// import { Directive, HostBinding, HostListener } from "@angular/core";
+
+// @Directive({
+//   selector: '[appDropdown]'
+// })
+
+// export class DropdownDirective {
+//   @HostBinding('class.open') isOpen: boolean = false;
+
+//   @HostListener('click') toggleOpen() {
+//     this.isOpen = !this.isOpen;
+//   }
+// }
+
+
+/////////////////////////////////////////
+//       Dropdown close anywhere       //
+/////////////////////////////////////////
+
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) { }
+}
