80bddcc4775ab5f888dc54134f59c8f6b6ef25c8
Tiago Ribeiro <tiagofr98@gmail.com>
2021-02-09 14:19:14 -0300

    Componentes dinâmicos
    Alerta de erro no login, criado dinamicamente

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
+import { AlertComponent } from '../shared/alert/alert.component'
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
   isLoading = false;
   isLoginMode = true;
   error: string = null;
+  private closeSub: Subscription;
+  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    if (this.closeSub)
+      this.closeSub.unsubscribe();
   }
 
   onSwitchMode() {
@@ -43,10 +52,29 @@ export class AuthComponent implements OnInit {
       this.router.navigate(['/recipes']);
     }, errorResponse => {
       console.log(errorResponse);
+      this.error = errorResponse;
+      this.showErrorAlert(errorResponse);
       this.isLoading = false;
     });
 
     form.reset();
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  private showErrorAlert(error: string) {
+    const alertFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const hostViewContainer = this.alertHost.viewContainerRef;
+    hostViewContainer.clear();
+
+    const componentRef = hostViewContainer.createComponent(alertFactory);
+    componentRef.instance.message = error;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainer.clear();
+    });
   }
 
 }
