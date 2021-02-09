bddaa7da431928a38750517edb626950bdb42939
Tiago Ribeiro <tiagofr98@gmail.com>
2021-02-09 10:24:19 -0300

    Autenticação e logout II
    Finalização dos métodos de autenticação e logout, validação e armazenamento de token etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'projeto-angular';
-  loadedFeature = 'recipe';
+export class AppComponent implements OnInit {
+  // title = 'projeto-angular';
+  // loadedFeature = 'recipe';
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.autoSignIn();
+  }
 
   // Método antigo para realizar a navegação
   // onNavigate(feature: string) {
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -20,6 +20,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -47,9 +48,41 @@ export class AuthService {
       ).pipe(catchError(this.handleError), tap(resData => this.handleAuth(resData.email, resData.localId, resData.idToken, +resData.expiresIn)));
   }
 
+  autoSignIn() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData)
+      return;
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer)
+      clearTimeout(this.tokenExpirationTimer);
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 
   private handleAuth(email: string, userId: string, token: string, expiresIn: number) {
@@ -61,6 +94,8 @@ export class AuthService {
       expirationDate
     );
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
